2b506c895f9f74dadfdbda5e39d3f98770424e84
Mario Demorez <robertreed632@gmail.com>
2026-07-24 17:47:16 -0500

    Added a pass through for resources

---
+++ src/haven/mario/theme/MarioTheme.java
@@ -0,0 +1,12 @@
+package haven.mario.theme;
+
+import java.awt.Color;
+
+public final class MarioTheme {
+
+    private MarioTheme() {
+    }
+
+    public static final Color WINDOW_BACKGROUND = new Color(0, 0, 128, 128);
+    public static final Color WINDOW_BORDER = new Color(0, 0, 0, 255);
+}
